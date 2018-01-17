a21d81d6d8eb8a20e1c6948bfdfd7276b8707a8a
aspea002 <andrew.spear@uscellular.com>
2018-01-16 19:04:17 -0600

    Add support for POST and DELETE for FUT API

--- resources/fut.py
+++ resources/fut.py
@@ -1,11 +1,17 @@
+import sys
 from flask import jsonify
 from flask_restful import Resource
 from common.common import Common
 
 
 class FUT(Resource):
-    """~
     """
+    """
+    if sys.argv[1] != '--dev':
+        fut_subscribers_file = '/opt/app-root/src/data_only/FUT-Subscribers'
+    else:
+        fut_subscribers_file = '/home/aspea002/IdeaProjects/USCC_ENG_API/data_only/FUT-subscribers'
+
     @staticmethod
     def get():
         """
@@ -15,8 +21,7 @@ class FUT(Resource):
 
         list_o_subscriber_ids = []
         dict_of_subscribers = {}
-        fut_subscribers_file = '/home/aspea002/IdeaProjects/USCC_ENG_API/data_only/FUT-subscribers'
-        with open(fut_subscribers_file) as fut_fh:
+        with open(FUT.fut_subscribers_file) as fut_fh:
             for line in fut_fh:
                 line = line.rstrip('\n')
                 list_o_subscriber_ids.append(line)
@@ -28,5 +33,48 @@ class FUT(Resource):
             dict_of_subscribers[list_index] = list_o_subscriber_ids[list_index]
 
         response = jsonify(dict_of_subscribers)
+        response.status_code = 200
+        return response
+
+    @staticmethod
+    def post():
+        """
+
+        :return:
+        """
+
+        uscc_eng_parser = Common.create_api_parser()
+        uscc_eng_parser.add_argument('imsi', location='json')
+        args = Common.parse_request_args(uscc_eng_parser)
+        list_imsi = args.get('imsi').split(',')
+        with open(FUT.fut_subscribers_file, "a") as sfh:
+            for imsi in list_imsi:
+                sfh.write(imsi + "\n")
+
+        response = jsonify({'fut_msg': 'IMSI(s) successfully added'})
+        response.status_code = 201
+        return response
+
+    @staticmethod
+    def delete():
+        """
+
+        :return:
+        """
+
+        uscc_eng_parser = Common.create_api_parser()
+        uscc_eng_parser.add_argument('imsi', location='json')
+        args = Common.parse_request_args(uscc_eng_parser)
+        list_imsi = args.get('imsi').split(',')
+        with open(FUT.fut_subscribers_file, "r") as sfhr:
+            lines = sfhr.readlines()
+            sfhr.close()
+        with open(FUT.fut_subscribers_file, "w") as sfhw:
+            for imsi_delete in list_imsi:
+                for imsi in lines:
+                    if imsi.strip("\n") != imsi_delete:
+                        sfhw.write(imsi)
+
+        response = jsonify({'fut_msg': 'IMSI(s) successfully delete'})
         response.status_code = 200
         return response
