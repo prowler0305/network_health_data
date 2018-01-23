3b7848d7f972eb3a5544fa1fa230b580714f4eff
aspea002 <Andrew.Spear@uscellular.com>
2018-01-23 10:57:10 -0600

    Set path to FUT-Subscribers while in OpenShift environment

--- resources/fut.py
+++ resources/fut.py
@@ -8,12 +8,10 @@ class FUT(Resource):
     """
     """
     try:
-        if sys.argv[1] != '--dev':
-            fut_subscribers_file = '/opt/app-root/src/data_only/FUT-Subscribers'
-        else:
+        if sys.argv[1] == '--dev':
             fut_subscribers_file = '/home/aspea002/IdeaProjects/USCC_ENG_API/data_only/FUT-subscribers'
     except IndexError:
-        fut_subscribers_file = '/opt/app-root/src/data_only/FUT-Subscribers'
+        fut_subscribers_file = '/opt/app-root/data-only/FUT-Subscribers'
 
     @staticmethod
     def get():
