f1950b614eeada0bb35036d2874498f99561f6e9
aspea002 <Andrew.Spear@uscellular.com>
2018-01-25 11:37:00 -0600

    Change sitemap api endpoint logic to allow for easy additions to endpoint keywords and descriptions to be added to the service
    Remove old wng_api_manager endpoint design

--- resources/site_map.py
+++ resources/site_map.py
@@ -14,18 +14,18 @@ class SiteMap(Resource):
         :return: standard HTTP response
         """
 
-        endpoint_dict = {}
+        endpoint_dict = dict(row_query='Query the database name requesting for a single row of data',
+                             list_all='List all the tables in the database name requested',
+                             action='Query information about a specific table in the database requests. (E.g. schema)'
+                             )
+
+        endpoint_url_dict = {}
         uscc_eng_app = flask.current_app
         for rule in uscc_eng_app.url_map.iter_rules():
             url = rule.rule
-            if 'row_query' in url:
-                endpoint_dict[url] = 'Query the database name requesting for a single row of data'
-            elif 'list_all' in url:
-                endpoint_dict[url] = 'List all the tables in the database name requested'
-            elif 'action' in url:
-                endpoint_dict[url] = 'Query information about a specific table in the database requests. (E.g. schema)'
-            else:
-                continue
+            for endpoint_key, endpoint_description in endpoint_dict.items():
+                if endpoint_key in url:
+                    endpoint_url_dict[url] = endpoint_description
 
         response = jsonify(endpoint_dict)
         response.status_code = 200
