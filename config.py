e58260759db573ab0ddd15f7221030323449bf4f
aspea002 <Andrew.Spear@uscellular.com>
2018-05-22 18:35:04 -0500

    Configuration file based changes

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+import datetime
+
+uscc_app_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+class BaseConfig:
+    SECRET_KEY = os.environ.get('USCC_SECRET_KEY') or 'you-will-never-guess'
+    JWT_SECRET_KEY = os.environ.get('USCC_JWT_KEY') or 'super-secret'
+    JWT_HEADER_TYPE = 'JWT'
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    PROPAGATE_EXCEPTIONS = False
+    if os.environ.get('access_token_expiration') is not None:
+        JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(seconds=int(os.environ.get('access_token_expiration')))
+    if os.environ.get('refresh_token_expiration') is not None:
+        JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(seconds=int(os.environ.get('refresh_token_expiration')))
+
+
+class QaConfig(BaseConfig):
+    DEBUG = os.environ.get('DEBUG') or False
+    PROPAGATE_EXCEPTIONS = os.environ.get('PROP_EXCPS') or False
+    if os.environ.get('access_token_expiration') is not None:
+        JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(seconds=int(os.environ.get('access_token_expiration')))
+    if os.environ.get('refresh_token_expiration') is not None:
+        JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(seconds=int(os.environ.get('refresh_token_expiration')))
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    PROPAGATE_EXCEPTIONS = True
+    if os.environ.get('access_token_expiration') is not None:
+        JWT_ACCESS_EXP = datetime.timedelta(seconds=int(os.environ.get('access_token_expiration')))
+    if os.environ.get('refresh_token_expiration') is not None:
+        JWT_REFRESH_EXP = datetime.timedelta(seconds=int(os.environ.get('refresh_token_expiration')))
