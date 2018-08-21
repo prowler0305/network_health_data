45cc995fdf153663fb11b9710f551f8b296e17c9
aspea002 <Andrew.Spear@uscellular.com>
2018-08-20 19:15:20 -0500

    Remove format on start_date DateField to take flask default to attempt to solve bug using in container environment with modern browser version

--- uscc_apps/dev_container_status/forms.py
+++ uscc_apps/dev_container_status/forms.py
@@ -4,7 +4,7 @@ from wtforms import StringField, SubmitField, DateField, RadioField
 
 class ContainerForm(FlaskForm):
     container_name = RadioField('', choices=[('dev_container_1', 'Dev Container 1'), ('dev_container_2', 'Dev Container 2')])
-    start_date = DateField('Start Date', format='%m/%d/%Y')
+    start_date = DateField('Start Date')
     end_date = DateField('End Date', format='%m/%d/%Y')
     poc_name = StringField('Name')
     poc_email = StringField('Email')
