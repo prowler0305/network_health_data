24fe5b5f2a8817584734a2be9f670b4a55a16ffa
aspea002 <Andrew.Spear@uscellular.com>
2018-08-20 19:28:27 -0500

    Fix issue with customized date format not recognized with modern up to date browsers. HTML5 type='date' employs a date picker that puts in form of yyyy-m-d format behind the scenese so Flask-WTForms has to use same format (which is the default) instead of using the format= parameter on DateField()

--- uscc_apps/dev_container_status/forms.py
+++ uscc_apps/dev_container_status/forms.py
@@ -5,7 +5,7 @@ from wtforms import StringField, SubmitField, DateField, RadioField
 class ContainerForm(FlaskForm):
     container_name = RadioField('', choices=[('dev_container_1', 'Dev Container 1'), ('dev_container_2', 'Dev Container 2')])
     start_date = DateField('Start Date')
-    end_date = DateField('End Date', format='%m/%d/%Y')
+    end_date = DateField('End Date')
     poc_name = StringField('Name')
     poc_email = StringField('Email')
     submit = SubmitField('Submit')
