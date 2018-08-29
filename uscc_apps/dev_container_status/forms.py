956aeb4fb19b14df74738e376ab816734b051f2f
aspea002 <Andrew.Spear@uscellular.com>
2018-08-29 16:55:48 -0500

    Add check box option to update container status data to an "open" status.

--- uscc_apps/dev_container_status/forms.py
+++ uscc_apps/dev_container_status/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField, RadioField
+from wtforms import StringField, SubmitField, DateField, RadioField, BooleanField
 
 
 class ContainerForm(FlaskForm):
     container_name = RadioField('', choices=[('dev_container_1', 'Dev Container 1'), ('dev_container_2', 'Dev Container 2')])
+    open_check = BooleanField(default=False)
     start_date = DateField('Start Date')
     end_date = DateField('End Date')
     poc_name = StringField('Name')
--- uscc_apps/dev_container_status/update_container_status.py
+++ uscc_apps/dev_container_status/update_container_status.py
@@ -58,62 +58,54 @@ class UpdateContainer(MethodView):
         #     self.imsi_header['content_type'] = 'application/json'
 
         form = ContainerForm()
-        if form.validate_on_submit():
-            with open(os.environ.get('container_status_path')) as csrfh:
-                status_file_dict = json.load(csrfh)
 
-            update_container_dict = status_file_dict.get(request.form.get('container_name'))
-            update_container_dict['status'] = 'not open'
-            update_container_dict.get('time_frame')['start_date'] = request.form.get('start_date')
-            update_container_dict.get('time_frame')['end_date'] = request.form.get('end_date')
-            update_container_dict.get('poc')['name'] = request.form.get('poc_name')
-            update_container_dict.get('poc')['email'] = request.form.get('poc_email')
-            status_file_dict[request.form.get('container_name')] = update_container_dict
-
-            with open(os.environ.get('container_status_path'),mode='w') as cswfh:
-                json.dump(status_file_dict, cswfh)
+        if request.form.get('open_status') == 'on':
+            self.open_container_status()
         else:
-            if len(form.errors) != 0:
-                for form_field, error_message_text in form.errors.items():
-                    Common.create_flash_message(message=form_field + ':' + error_message_text[0])
+            if form.validate_on_submit():
+                with open(os.environ.get('container_status_path')) as csrfh:
+                    status_file_dict = json.load(csrfh)
+
+                update_container_dict = status_file_dict.get(request.form.get('container_name'))
+                update_container_dict['status'] = 'not open'
+                update_container_dict.get('time_frame')['start_date'] = request.form.get('start_date')
+                update_container_dict.get('time_frame')['end_date'] = request.form.get('end_date')
+                update_container_dict.get('poc')['name'] = request.form.get('poc_name')
+                update_container_dict.get('poc')['email'] = request.form.get('poc_email')
+                status_file_dict[request.form.get('container_name')] = update_container_dict
+
+                with open(os.environ.get('container_status_path'),mode='w') as cswfh:
+                    json.dump(status_file_dict, cswfh)
+            else:
+                if len(form.errors) != 0:
+                    for form_field, error_message_text in form.errors.items():
+                        Common.create_flash_message(message=form_field + ':' + error_message_text[0])
 
         return render_template('container_status/update_container_status.html', form=form)
 
-    def delete(self):
+    def open_container_status(self):
         """
-        Called by the above post() method if the radio button value is 'D' in which an HTTP DELETE request is made to
-        the USCC ENG REST API to delete the imsi(s) from the tracking file.
+        If the "Set to open status" checkbox is checked for a container name, clears the dictionary information for the
+        container_name and sets the "status" key to "open".
 
-        :return: True - if the delete API request is successful with a successful message
-                False - if the delete API request is unsuccessful with an error message derived from the API HTTP
-                        Response object.
+        :return:
         """
 
-        if 'logout_btn' in request.form:
-            self.imsi_header['X-CSRF-TOKEN'] = request.cookies.get('csrf_access_token')
-            imsi_logout_resp = requests.delete(api.url_for(Logout, _external=True), headers=self.imsi_header,
-                                               cookies={'access_token_cookie': request.cookies.get('access_token_cookie')})
-            if imsi_logout_resp.status_code == requests.codes.ok:
-                self.redirect_to_uscc_login()
+        with open(os.environ.get('container_status_path'), 'r') as csrfh:
+            status_file_dict = json.load(csrfh)
 
-
-        else:
-            imsi_delete_resp = requests.delete(api.url_for(Imsi, _external=True),
-                                               data=json.dumps(self.imsi_tracking_dict),
-                                               headers=self.imsi_header,
-                                               cookies={'access_token_cookie': request.cookies.get('access_token_cookie')})
-
-            if imsi_delete_resp.status_code == requests.codes.ok:
-                Common.create_flash_message(imsi_delete_resp.json().get('imsi_msg'))
-                return True
+        update_container_dict = status_file_dict.get(request.form.get('container_name'))
+        for k, v in update_container_dict.items():
+            if k == 'status':
+                update_container_dict[k] = 'open'
             else:
-                if imsi_delete_resp.status_code == requests.codes.unauthorized:
-                    self.redirect_to_uscc_login()
-                    return self.login_redirect_response
-                else:
-                    delete_error_message = "%s: %s" % (imsi_delete_resp.status_code, imsi_delete_resp.reason)
-                    Common.create_flash_message(delete_error_message, 'error')
-                    return False
+                for sub_dict_key in update_container_dict.get(k).keys():
+                    update_container_dict.get(k)[sub_dict_key] = ""
+
+        status_file_dict[request.form.get('container_name')] = update_container_dict
+
+        with open(os.environ.get('container_status_path'),mode='w') as cswfh:
+            json.dump(status_file_dict, cswfh)
 
     def redirect_to_uscc_login(self):
         """
